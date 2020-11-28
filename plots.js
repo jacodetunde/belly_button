5cb3e63aa708a0cbbf8517502f31ea9f878270a9
jacodetunde <65673344+jacodetunde@users.noreply.github.com>
2020-11-28 04:47:24 -0600

    Adding updates

--- plots.js
+++ plots.js
@@ -29,6 +29,6 @@ function buildMetadata(sample) {
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text(result);
+    PANEL.append("h6").text(result.location);
   });
 }
