694509bcb7adf0b52a599c347f298653d3e0654d
jacodetunde <65673344+jacodetunde@users.noreply.github.com>
2020-11-28 05:42:50 -0600

    Adding updates

--- plots.js
+++ plots.js
@@ -25,7 +25,7 @@ function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0,5];
+    var result = resultArray[0];
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
